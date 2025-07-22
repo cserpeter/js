c1c169e1ddc580dd1135264c786ac8a09b3e7e17
Cseresnyés Péter <csefid@gmail.com>
2025-07-22 23:26:51 +0200

    +1

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+  if (num < 3) return 1;
+  let solution = 1;
+  for (let i = 1; i < num; i++) {
+    solution += i;
+  }
+  return solution;
 };
 
 // Do not edit below this line
