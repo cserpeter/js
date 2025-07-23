36c81238bcb3177a27ef7bfc70bb4807ca91c382
Cseresnyés Péter <csefid@gmail.com>
2025-07-23 13:20:37 +0200

    finish all challenges

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,10 +1,19 @@
-const fibonacci = function (num) {
+const fibonacci = function (n) {
+  const num = typeof n === "string" ? parseInt(n) : n;
+  if (num < 0) return "OOPS";
+  if (num === 0) return 0;
   if (num < 3) return 1;
-  let solution = 1;
-  for (let i = 1; i < num; i++) {
-    solution += i;
+  const arr = [1, 1];
+  let newLastNum;
+  let newFirstNum;
+  for (let i = 2; i < num; i++) {
+    newLastNum = arr.reduce((acc, cur) => acc + cur);
+    newFirstNum = arr.pop();
+    arr[0] = newFirstNum;
+    arr[1] = newLastNum;
   }
-  return solution;
+
+  return arr[1];
 };
 
 // Do not edit below this line
--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (array) {
+  const date = new Date();
+  const ageArray = array.map((person) => {
+    if ("yearOfDeath" in person) return person.yearOfDeath - person.yearOfBirth;
+    else return date.getFullYear() - person.yearOfBirth;
+  });
+  const oldestAge = Math.max(...ageArray);
+  const oldestIndex = ageArray.indexOf(oldestAge);
+  return array[oldestIndex];
 };
 
 // Do not edit below this line
